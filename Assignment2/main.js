631b893994cf16d43e50f6bb51edb38e7f885914
Avery Turnquest <averyturnquest64@gmail.com>
2021-09-20 09:27:07 -0500

    update

---
+++ Assignment2/main.js
@@ -0,0 +1,61 @@
+//Question 1
+console.log("Question 1");
+let myNumber = 0;
+console.log(myNumber);
+
+//Question 2
+console.log("Question 2");
+myNumber = 5;
+console.log("Type of myNumber: " + typeof(myNumber));
+console.log("Value of myNumber: " + myNumber);
+
+//Question 3
+console.log("Question 3");
+let myOtherNumber = 5;
+console.log("Type of myOtherNumber: " + typeof(myOtherNumber));
+console.log("Value of myOtherNumber: " + myOtherNumber);
+
+//Question 4
+console.log("Question 4");
+let myTotal = myNumber * myOtherNumber;
+console.log("Type of myTotal: " + typeof(myTotal));
+console.log("Product of myNumber and myOtherNumber: " + myTotal);
+
+//Question 5
+console.log("Question 5");
+let myName = "Avery";
+let myCombo = String(myNumber + myOtherNumber);
+console.log("Type of myName: " + typeof(myName));
+console.log("Value of myName: " + myName);
+console.log("Type of myCombo: " + typeof(myCombo));
+console.log("Value of myCombo: " + myCombo);
+
+//Question 6
+console.log("Question 6");
+let myOtherCombo = myNumber + myOtherNumber;
+console.log("Type of myOtherCombo: " + typeof(myOtherCombo));
+console.log("Sum of myNumber and myOtherNumber: " + myOtherCombo);
+
+//Question 7
+console.log("Question 7");
+let myRemainder = myNumber%5;
+console.log("Type of myRemainder: " + typeof(myRemainder));
+console.log("Remainder of myNumber divided by 5: " + myRemainder);
+
+//Question 8
+console.log("Question 8");
+let i = 2;
+console.log("Type of i: " + typeof(i));
+console.log("Value of i: " + i);
+
+//Question 9
+console.log("Question 9");
+let int = 4;
+console.log("Type of int: " + typeof(int));
+console.log("Value of int: " + int);
+
+//Question 10
+console.log("Question 10");
+let myBoolean = false;
+console.log("Type of myBoolean: " + typeof(myBoolean));
+console.log("Value of myBoolean: " + myBoolean);
